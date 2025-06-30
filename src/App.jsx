f30ad894de0ca418f2446a8c4c7fab083267f252
DevAlmisawee <muhammadalmisawee@gmail.com>
2025-06-30 15:26:41 +0100

    secondCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router"
+import { 
+  Route, 
+  RouterProvider, 
+  createHashRouter,  // Use createHashRouter instead of createBrowserRouter
+  createRoutesFromElements 
+} from "react-router-dom";  // Make sure to import from "react-router-dom"
 import MainLayOut from "./Layout/MainLayOut";
 import TableContent from "./Component/TableContent";
 import './CSS/Style.css';
 
-
 function App() {
- 
-  const router = createBrowserRouter(
+  const router = createHashRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayOut />}>
         <Route index element={<TableContent />} />
       </Route>
     )
   );
+  
   return (
     <>
-      {/* <CoinInfo /> */}
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
